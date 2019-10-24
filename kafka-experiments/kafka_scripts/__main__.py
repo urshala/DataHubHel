1a3efd9d5e98a1e1e67cb4217ab4faf14b3c5bcc
Tuomas Suutari <tuomas.suutari@anders.fi>
2019-10-24 17:32:23 +0300

    kafka_scripts: Add main entrypoint script
    Add a script than can be used to launch the scripts in this package.

---
+++ kafka-experiments/kafka_scripts/__main__.py
@@ -0,0 +1,30 @@
+import argparse
+import sys
+
+from . import initialize_services, open311_consumer, sample_producer
+
+COMMAND_MAP = {
+    "init": initialize_services.main,
+    "produce-sample-data": sample_producer.produce_sample_data,
+    "consume-open311": open311_consumer.listen_open311,
+}
+
+
+def main(argv=sys.argv):
+    args = parse_args(argv)
+    func = COMMAND_MAP[args.command]
+    func()
+
+
+def parse_args(argv):
+    parser = argparse.ArgumentParser(prog=argv[0])
+    subparsers = parser.add_subparsers(
+        title="command", dest="command", required=True
+    )
+    for (name, module) in COMMAND_MAP.items():
+        subparsers.add_parser(name)
+    return parser.parse_args(argv[1:])
+
+
+if __name__ == "__main__":
+    main()
