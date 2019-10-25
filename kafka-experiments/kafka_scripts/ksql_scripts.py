5a33a6d4e117dcec44121d624a5f1f9b40d4152e
Tuomas Suutari <tuomas.suutari@anders.fi>
2019-10-25 17:54:09 +0300

    kafka_scripts: Clean-up ksql_scripts
    * Move the main function to top and utility functions to the bottom.
    
    * Remove redundant comments, since the docstrings should be enough.
    
    * Format the docstrings to separated title and body format.
    
    * Convert function names to lower case.

--- kafka-experiments/kafka_scripts/ksql_scripts.py
+++ kafka-experiments/kafka_scripts/ksql_scripts.py
@@ -13,6 +13,113 @@ headers = {
 }
 
 
+def create_ksql_streams():
+    _create_noise_stream()
+    _create_sensor_name_keyed_stream()
+    _create_loud_noise_stream()
+    _create_min_value_table()
+    _create_open311_topic()
+    _create_location_based_steam()
+
+
+def _create_noise_stream():
+    """
+    Create the base stream from the noise topic.
+
+    This is the base of all other topics/streams/table
+    """
+    if _has_stream(settings.NOISE_STREAM):
+        return
+
+    _execute_ksql_commands(
+        f"CREATE STREAM {settings.NOISE_STREAM}"
+        f" WITH (kafka_topic='{settings.KAFKA_TOPIC}',"
+        f" value_format='{settings.VALUE_FORMAT}');")
+    LOG.info('Created KSQL stream: %s', settings.NOISE_STREAM)
+
+
+def _create_location_based_steam():
+    """
+    Create the stream with location for elasticsearch
+    """
+    if _has_stream('ELASTIC_LOCATION_STREAM'):
+        return
+
+    _execute_ksql_commands(
+        f"CREATE STREAM ELASTIC_LOCATION_STREAM"
+        f" AS SELECT SENSOR->SENSOR_NAME AS SENSOR_NAME,"
+        f" THING->LOCATION AS LOCATION"
+        f" FROM {settings.NOISE_STREAM}"
+        f" PARTITION BY SENSOR_NAME;")
+    LOG.info('Created KSQL stream: %s', 'ELASTIC_LOCATION_STREAM')
+
+
+def _create_sensor_name_keyed_stream():
+    """
+    Create the stream with key (sensor_name).
+
+    The key part is required if we want to save the message to database.
+    """
+    if _has_stream(settings.NOISE_STREAM_KEYED):
+        return
+
+    _execute_ksql_commands(
+        f"CREATE STREAM {settings.NOISE_STREAM_KEYED}"
+        f" AS SELECT SENSOR->SENSOR_NAME AS SENSOR_NAME,"
+        f" RESULTS->LEVEL AS LEVEL,"
+        f" RESULTS->BATTERY AS BATTERY,"
+        f" RESULTS->POWER AS POWER,"
+        f" RESULTS->OVERLOAD AS OVERLOAD,"
+        f" THING->THING_NAME AS THING_NAME,"
+        f" THING->LOCATION[0] AS LON,"
+        f" THING->LOCATION[1] AS LAT"
+        f" FROM {settings.NOISE_STREAM}"
+        f" PARTITION BY SENSOR_NAME;")
+    LOG.info('Created KSQL stream: %s', settings.NOISE_STREAM_KEYED)
+
+
+def _create_min_value_table():
+    if _has_table(settings.MIN_VALUE_TABLE):
+        return
+
+    _check_stream_create_status(settings.NOISE_STREAM_KEYED)
+    _execute_ksql_commands(
+        f"CREATE TABLE {settings.MIN_VALUE_TABLE} AS"
+        f" SELECT SENSOR_NAME, MIN(BATTERY) AS BATTERY FROM"
+        f" {settings.NOISE_STREAM_KEYED} GROUP BY sensor_name;")
+    LOG.info('Created KSQL table: %s', settings.MIN_VALUE_TABLE)
+
+
+def _create_open311_topic():
+    """
+    Create a stream that captures noise levels over 7.0.
+    """
+    if _has_stream(settings.ALERT_TOPIC):
+        return
+
+    _check_stream_create_status(settings.NOISE_STREAM)
+    _execute_ksql_commands(
+        f"CREATE STREAM {settings.ALERT_TOPIC} AS"
+        f" SELECT * FROM {settings.NOISE_STREAM} WHERE"
+        f" RESULTS->LEVEL > 7.0 AND RESULTS->OVERLOAD = True;")
+    LOG.info('Created KSQL stream: %s', settings.ALERT_TOPIC)
+
+
+def _create_loud_noise_stream():
+    """
+    Create a stream that captures noise levels over 4.0.
+    """
+    if _has_stream(settings.LOUD_NOISE_TOPIC):
+        return
+
+    _check_stream_create_status(settings.NOISE_STREAM)
+    _execute_ksql_commands(
+        f"CREATE STREAM {settings.LOUD_NOISE_TOPIC}"
+        f" AS SELECT * from {settings.NOISE_STREAM_KEYED}"
+        f" WHERE LEVEL > 4.0;")
+    LOG.info('Created KSQL stream: %s', settings.LOUD_NOISE_TOPIC)
+
+
 def _has_stream(name):
     return _has_object('stream', name)
 
@@ -49,106 +156,6 @@ def _check_stream_create_status(stream=None):
     url = f'{settings.KSQL_URL}/status/stream/{stream}/create'
     response = requests.get(url)
     assert response.json()['status'] == 'SUCCESS'
-
-
-def _create_noise_stream():
-    """
-    Create the base stream from the noise topic.This
-    is the base of all other topics/streams/table
-    """
-    if _has_stream(settings.NOISE_STREAM):
-        return
-
-    _execute_ksql_commands(
-        f"CREATE STREAM {settings.NOISE_STREAM}"
-        f" WITH (kafka_topic='{settings.KAFKA_TOPIC}',"
-        f" value_format='{settings.VALUE_FORMAT}');")
-    LOG.info('Created KSQL stream: %s', settings.NOISE_STREAM)
-
-
-def _create_location_based_steam():
-    """
-    Create the stream with location for elasticsearch
-    """
-    if _has_stream('ELASTIC_LOCATION_STREAM'):
-        return
-
-    _execute_ksql_commands(
-        f"CREATE STREAM ELASTIC_LOCATION_STREAM"
-        f" AS SELECT SENSOR->SENSOR_NAME AS SENSOR_NAME,"
-        f" THING->LOCATION AS LOCATION"
-        f" FROM {settings.NOISE_STREAM}"
-        f" PARTITION BY SENSOR_NAME;")
-    LOG.info('Created KSQL stream: %s', 'ELASTIC_LOCATION_STREAM')
-
-
-def _create_sensor_name_keyed_stream():
-    """
-    Create the stream with key (sensor_name). The key part is required
-    if we want to save the message to database.
-    """
-    if _has_stream(settings.NOISE_STREAM_KEYED):
-        return
-
-    _execute_ksql_commands(
-        f"CREATE STREAM {settings.NOISE_STREAM_KEYED}"
-        f" AS SELECT SENSOR->SENSOR_NAME AS SENSOR_NAME,"
-        f" RESULTS->LEVEL AS LEVEL,"
-        f" RESULTS->BATTERY AS BATTERY,"
-        f" RESULTS->POWER AS POWER,"
-        f" RESULTS->OVERLOAD AS OVERLOAD,"
-        f" THING->THING_NAME AS THING_NAME,"
-        f" THING->LOCATION[0] AS LON,"
-        f" THING->LOCATION[1] AS LAT"
-        f" FROM {settings.NOISE_STREAM}"
-        f" PARTITION BY SENSOR_NAME;")
-    LOG.info('Created KSQL stream: %s', settings.NOISE_STREAM_KEYED)
-
-
-def _create_min_value_table():
-    if _has_table(settings.MIN_VALUE_TABLE):
-        return
-
-    _check_stream_create_status(settings.NOISE_STREAM_KEYED)
-    _execute_ksql_commands(
-        f"CREATE TABLE {settings.MIN_VALUE_TABLE} AS"
-        f" SELECT SENSOR_NAME, MIN(BATTERY) AS BATTERY FROM"
-        f" {settings.NOISE_STREAM_KEYED} GROUP BY sensor_name;")
-    LOG.info('Created KSQL table: %s', settings.MIN_VALUE_TABLE)
-
-
-def _create_OPEN311_topic():
-    if _has_stream(settings.ALERT_TOPIC):
-        return
-
-    _check_stream_create_status(settings.NOISE_STREAM)
-    _execute_ksql_commands(
-        f"CREATE STREAM {settings.ALERT_TOPIC} AS"
-        f" SELECT * FROM {settings.NOISE_STREAM} WHERE"
-        f" RESULTS->LEVEL > 7.0 AND RESULTS->OVERLOAD = True;")
-    LOG.info('Created KSQL stream: %s', settings.ALERT_TOPIC)
-
-
-def _create_loud_noise_stream():
-    if _has_stream(settings.LOUD_NOISE_TOPIC):
-        return
-
-    _check_stream_create_status(settings.NOISE_STREAM)
-    _execute_ksql_commands(
-        f"CREATE STREAM {settings.LOUD_NOISE_TOPIC}"
-        f" AS SELECT * from {settings.NOISE_STREAM_KEYED}"
-        f" WHERE LEVEL > 4.0;")
-    LOG.info('Created KSQL stream: %s', settings.LOUD_NOISE_TOPIC)
-
-
-def create_ksql_streams():
-    _create_noise_stream()  # Registers a stream in ksql for noise topic.
-    _create_sensor_name_keyed_stream() #  Registers a base keyed stream
-    _create_loud_noise_stream()  # Registers and runs a stream for where level > theshold value also save to database.
-    _create_min_value_table()
-    _create_OPEN311_topic()  # Registers and runs stream where level > threshold value.
-    _create_location_based_steam()
-
 
 
 if __name__ == '__main__':
