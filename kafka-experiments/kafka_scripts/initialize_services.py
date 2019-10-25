f8e0344da4c589f654d83e496c667ec839f364d5
Tuomas Suutari <tuomas.suutari@anders.fi>
2019-10-25 11:25:47 +0300

    kafka_scripts/init: Remove outdated parts
    The comment is not correct anymore, so remove it.
    
    These functions are not intended to be run as scripts anymore but rather
    via the __main__.py or via "python -m kafka_scripts.module_name", so
    remove the executable bits and shebangs.

--- kafka-experiments/kafka_scripts/initialize_services.py
+++ kafka-experiments/kafka_scripts/initialize_services.py
@@ -1,4 +1,3 @@
-#!/usr/bin/env python3
 from .elastic_search_geopoint_template import map_lat_lng_to_geopoints
 from .ksql_scripts import create_ksql_streams
 from .load_kafka_connectors import load_kafka_connectors
@@ -11,13 +10,4 @@ def main():
 
 
 if __name__ == '__main__':
-    """
-        This does the following stuffs:
-        1. Maps lat, lng to geopoint in elasticsearch
-        1. Creates the streams in the kafka
-        2. Loads the connectors needed to connect kafka to
-            elasticsearch, postgres database for sinking data.
-        3. Starts the sample consumer that listens to OPEN311
-            topic
-    """
     main()
