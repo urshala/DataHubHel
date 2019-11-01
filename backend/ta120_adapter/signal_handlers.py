dd071db6239b047c12c164d04640c19352b4daf7
Deepak Panta <deepak.panta@anders.fi>
2019-11-01 12:32:41 +0200

    Make sure that many2many field on TA120Sensor get saved
    The datastream needs to be attached to each TA120Sensor upon
    creation. Since datastream is the M2M field, it threfore needs
    to exist before it can be attached to TA120Sensor. This is why
    we needed the transaction.commit so that the creating and attaching
    of datastreams to TA120Sensor works on post_save.

--- backend/ta120_adapter/signal_handlers.py
+++ backend/ta120_adapter/signal_handlers.py
@@ -6,16 +6,17 @@ from .properties import DATA_PROPERTIES
 
 
 def add_ta120sensor_datastreams(
-        sender: Type[TA120Sensor],
-        instance: TA120Sensor,
-        **kwargs: object,
+    sender: Type[TA120Sensor], instance: TA120Sensor, **kwargs: object
 ):
-    sensor = instance
     for (key, prop) in DATA_PROPERTIES.items():
-        sensor.datastreams.get_or_create(
-            name=key, defaults={
-                'thing': sensor.thing,
-                'sensor': instance,
-                'sts_id': random.randint(1, 2**31 - 1), #TODO: Do something about this
-                'description': prop.description,
-            })
+        instance.datastreams.get_or_create(
+            name=key,
+            defaults={
+                "thing": instance.thing,
+                "sensor": instance.sensor,
+                "sts_id": random.randint(
+                    1, 2 ** 31 - 1
+                ),  # TODO: Do something about this
+                "description": prop.description,
+            },
+        )
