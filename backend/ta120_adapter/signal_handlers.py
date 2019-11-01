7ebb8dc8431175e44154ddeff46b474bc0782b92
Deepak Panta <deepak.panta@anders.fi>
2019-11-01 12:32:41 +0200

    Make sure that many2many field on TA120Sensor get saved
    The datastream needs to be attached to each TA120Sensor upon
    creation. Since datastream is the M2M field, it threfore needs
    to exist before it can be attached to TA120Sensor. This is why
    we needed the transaction.commit so that the creating and attaching
    of datastreams to TA120Sensor works on post_save.

--- backend/ta120_adapter/signal_handlers.py
+++ backend/ta120_adapter/signal_handlers.py
@@ -1,21 +1,36 @@
 import random
 from typing import Type
 
+from django.db import transaction
+
 from .models import TA120Sensor
 from .properties import DATA_PROPERTIES
 
 
+def on_transaction_commit(func):
+    def inner(*args, **kwargs):
+        transaction.on_commit(lambda: func(*args, **kwargs))
+
+    return inner
+
+
+# We need to make sure that datastream (M2MField) gets
+# correctly attached to TA120Sensor after it is commited to db.
+
+
+@on_transaction_commit
 def add_ta120sensor_datastreams(
-        sender: Type[TA120Sensor],
-        instance: TA120Sensor,
-        **kwargs: object,
+    sender: Type[TA120Sensor], instance: TA120Sensor, **kwargs: object
 ):
-    sensor = instance
     for (key, prop) in DATA_PROPERTIES.items():
-        sensor.datastreams.get_or_create(
-            name=key, defaults={
-                'thing': sensor.thing,
-                'sensor': instance,
-                'sts_id': random.randint(1, 2**31 - 1), #TODO: Do something about this
-                'description': prop.description,
-            })
+        instance.datastreams.get_or_create(
+            name=key,
+            defaults={
+                "thing": instance.thing,
+                "sensor": instance.sensor,
+                "sts_id": random.randint(
+                    1, 2 ** 31 - 1
+                ),  # TODO: Do something about this
+                "description": prop.description,
+            },
+        )
