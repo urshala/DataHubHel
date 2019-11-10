2593a000f05bb88ceecb077cf43b7355a64a5c7f
Deepak Panta <deepak.panta@anders.fi>
2019-11-10 16:29:49 +0200

    Add login route for the frontend.

--- backend/datahubhel/auth/urls.py
+++ backend/datahubhel/auth/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from rest_framework import routers
+from rest_framework.authtoken import views
 
 from . import api
 
@@ -8,6 +9,7 @@ router = routers.SimpleRouter()
 router.register('user-tokens', api.TokenViewSet, base_name='user-token')
 
 urlpatterns = router.urls + [
+    path('login/', views.obtain_auth_token, name='user-login'),
     path('me/', api.MeView.as_view(), name='personal-data'),
     path('me/register/', api.RegisterView.as_view(), name='register-user'),
     path('me/forget/', api.ForgetView.as_view(), name='forget-user'),
